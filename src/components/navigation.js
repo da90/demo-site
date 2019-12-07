a94bad077c28e36310545b53f3663cc889205a59
da90 <dmitrij1990@hotmail.com>
2019-12-07 17:22:59 +0000

    work on banner navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,16 +3,20 @@ import { Link } from "gatsby"
 
 const Navigation = () => <nav>
     <div>
-    <Link to="/account">Hello, Phil!</Link>
-    <Link to="/contact-us#help">Help</Link>
-    <Link to="/contact-us">Contact Us</Link>
+        <Link to="/account">Hello, Phil!</Link>
+        <Link to="/contact-us#help">Help</Link>
+        <Link to="/contact-us">Contact Us</Link>
     </div>
 
     <div>
-    <Link to="/">Home</Link>
-    <Link to="/dashboard">Dashboard</Link>
-    <Link to="/record-of-learning">Record of Learning</Link>
-    <Link to="/record-of-learning">Meet the team</Link>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/record-of-learning">Record of Learning</Link>
+        <Link to="/record-of-learning">Meet the team</Link>
+        <span>
+            <input type="text" />
+            <i class="fas fa-search"></i>
+        </span>
     </div>
 </nav>
 
