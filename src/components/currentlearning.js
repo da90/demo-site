a7b078d6d5003c7d52b5b742f77f713b036516a7
da90 <dmitrij1990@hotmail.com>
2019-12-08 17:20:30 +0000

    current learning done, mobile improvements

--- src/components/currentlearning.js
+++ src/components/currentlearning.js
@@ -9,7 +9,7 @@ const CurrentLearning = () => <section class="currentlearning">
         <div class="currentlearning__list-item">
             <div>
                 <p>Name</p>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
 
             <div>
@@ -24,6 +24,81 @@ const CurrentLearning = () => <section class="currentlearning">
             <div>
                 <p>Activities</p>
                 <p>5 of 10 completed</p>
+            </div>
+
+            <div>
+                <Link to="/dashboard" className="button button--secondary" title="Launch course">Launch Course</Link>
+            </div>
+        </div>
+
+        <div class="currentlearning__list-item">
+            <div>
+                <p>Name</p>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+
+            <div>
+                <p>Complete</p>
+                <p>No</p>
+            </div>
+
+            <div>
+                <Progress progress="90%"/>
+            </div>
+
+            <div>
+                <p>Activities</p>
+                <p>6 of 8 completed</p>
+            </div>
+
+            <div>
+                <Link to="/dashboard" className="button button--secondary" title="Launch course">Launch Course</Link>
+            </div>
+        </div>
+
+        <div class="currentlearning__list-item">
+            <div>
+                <p>Name</p>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
+
+            <div>
+                <p>Complete</p>
+                <p>No</p>
+            </div>
+
+            <div>
+                <Progress progress="75%"/>
+            </div>
+
+            <div>
+                <p>Activities</p>
+                <p>5 of 10 completed</p>
+            </div>
+
+            <div>
+                <Link to="/dashboard" className="button button--secondary" title="Launch course">Launch Course</Link>
+            </div>
+        </div>
+
+        <div class="currentlearning__list-item">
+            <div>
+                <p>Name</p>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div>
+                <p>Complete</p>
+                <p>No</p>
+            </div>
+
+            <div>
+                <Progress progress="10%"/>
+            </div>
+
+            <div>
+                <p>Activities</p>
+                <p>1 of 10 completed</p>
             </div>
 
             <div>
