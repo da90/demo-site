2e5b42eb94799d0b82d0ded6323e7c3637f80a2d
da90 <dmitrij1990@hotmail.com>
2019-12-08 13:58:50 +0000

    adjusted fonts, finished header

--- src/components/header.js
+++ src/components/header.js
@@ -35,7 +35,7 @@ const Header = ({ siteTitle }) => (
     <section>
     <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
     <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <Link to="/record-of-learning" className="button">Record of Learning</Link>
+    <Link to="/record-of-learning" className="button button--primary">Record of Learning</Link>
     </section>
     <Card/>  
   </header>
