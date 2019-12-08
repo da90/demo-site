f42bf2d8149325094a93c56961af61fb874edeb8
da90 <dmitrij1990@hotmail.com>
2019-12-08 13:30:48 +0000

    finishing card badges

---
+++ src/components/badges.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Badges = () => <section>
+    <i class="fas fa-gavel"></i>
+    <i class="fas fa-comments"></i>
+    <i class="fas fa-graduation-cap"></i>
+    <i class="fas fa-database"></i>
+    <i class="badge--empty"></i>
+</section>
+
+export default Badges
--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Image from "../components/image"
+import Badges from "../components/badges"
 
 const Card = (props) => <section className="card">
     <Image />
     <i class="far fa-envelope" title="E-mail Terry"></i>
     <div>
         <h3>Terry Morgan</h3>
-        <p>terry.morgan@centizu.com</p>
+        <a href="mailto:terry.morgan@centizu.com">terry.morgan@centizu.com</a>
         <p><strong>Last access to site:</strong></p>
         <p>Thursday, 5 October 2017</p>
         <h3>Badges</h3>
+        <Badges/>
     </div>
 </section>
 
